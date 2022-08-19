7aab8e8f6e10a13c4e114f8e1af8b1bc1fb413de
Darren Xu <darrenxu1994@gmail.com>
2022-08-20 08:21:55 +1000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://DarrenXu94.github.io",
+  base: "astro-docs",
+  integrations: [mdx(), sitemap()],
+});
