d205939f0c3a21fed08f6bd9bd3fd86475570786
Darren Xu <darrenxu1994@gmail.com>
2022-08-20 08:31:59 +1000

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://DarrenXu94.github.io",
-  base: "astro-docs",
+  base: "/astro-docs",
   integrations: [mdx(), sitemap()],
 });
