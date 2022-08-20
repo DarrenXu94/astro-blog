55c0e3902df81ea0eb22e4d53eaaa5165b254be5
Darren Xu <darrenxu1994@gmail.com>
2022-08-20 11:17:34 +1000

    Astro image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://DarrenXu94.github.io",
   base: "/astro-docs",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), image()]
 });
